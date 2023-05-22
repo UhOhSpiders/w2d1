ba48d8a594fc5fc3109bddbe6630621a3a7e2ece
UhOhSpiders <iain.mccall.95@gmail.com>
2023-05-22 15:46:24 +0100

    part 1 complete

--- single_class_lab_start_code/student_class/src/student.py
+++ single_class_lab_start_code/student_class/src/student.py
@@ -1,1 +1,21 @@
+class Student:
+    def __init__(self, name, cohort):
+        self.name = name
+        self.cohort = cohort
+        # self.talk = talk
+        # self.talk = talk
+        # self.fav_lang = fav_lang
+    
+    def name(self, name):
+        self.name = name
 
+    def cohort(self, cohort):
+        self.cohort = cohort
+    
+    def talk(self):
+        # self.talk = talk
+        return "I can talk!"
+    
+    def say_favourite_language(self, fav):
+        say_fav = "I love " + fav
+        return say_fav
