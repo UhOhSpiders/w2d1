af9aaa326fea188b026f3229701d35bc4be86950
UhOhSpiders <iain.mccall.95@gmail.com>
2023-05-22 16:26:34 +0100

    mvp reached

---
+++ single_class_lab_start_code/team_class/src/team.py
@@ -0,0 +1,17 @@
+#THE ARGUMENTS ALWAYS START WITH SELF
+
+class Team:
+    def __init__(self, name, players, coach):
+        # breakpoint()
+        self.name = name
+        self.players = players
+        self.coach = coach
+    
+    def add_player(self, new_player):
+        self.players.append(new_player)
+
+    def has_player(self, player):
+        if player in self.players: 
+            return True
+        else: 
+            return False
